be30800a6981b36d68b080a9b82a97a9e5e9a0e5
Marlon Cruz <marlomsilvacruz@gmail.com>
2019-10-29 16:38:06 -0300

    Create Complexity_analysis.c
    Iniciando projeto

---
+++ Complexity_analysis.c
@@ -0,0 +1,42 @@
+///Análise de complexidade algorítmica
+///APS 4º Semestre - UNIP Marquês
+///autor: marlon cruz
+///data de criação: 15/10/2019
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+typedef struct Registro{
+	//##
+	char[100] desktopID; //ID da máquina a qual está sendo executado o código
+	char[100] sortAlgorithmName; //nome do algorítmo de ordenação 
+	int comparisons; //total de comparações realizadas na ordenação
+	int arrayAccesses; //total de modificações na estrutura do vetor
+	int totalTime; //tempo total para ordenação medido em unidades de segundo	
+};
+
+int[] getArray(int n){
+	
+}
+
+
+
+
+
+//variáveis de escopo global
+
+
+int main(){
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	return(1);
+}
